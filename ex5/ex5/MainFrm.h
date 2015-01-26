8bc09303cb5e3b2ff8fdc6c9a41789f27180a67a
just-dj- <690932223@qq.com>
2015-01-26 19:46:44 +0800

    mainfrm.h

--- ex5/ex5/MainFrm.h
+++ ex5/ex5/MainFrm.h
@@ -1,27 +1,24 @@
-// MainFrm.h : CMainFrame ÀàµÄ½Ó¿Ú
-//
-
-
+// MainFrm.h : CMainFrame ç±»çš„æ¥å£
 #pragma once
 
 class CMainFrame : public CFrameWnd
 {
 	
-protected: // ½ö´ÓĞòÁĞ»¯´´½¨
+protected: // ä»…ä»åºåˆ—åŒ–åˆ›å»º
 	CMainFrame();
 	DECLARE_DYNCREATE(CMainFrame)
 
-// ÊôĞÔ
+// å±æ€§
 public:
 
-// ²Ù×÷
+// æ“ä½œ
 public:
 
-// ÖØĞ´
+// é‡å†™
 public:
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 
-// ÊµÏÖ
+// å®ç°
 public:
 	virtual ~CMainFrame();
 #ifdef _DEBUG
@@ -29,11 +26,11 @@ public:
 	virtual void Dump(CDumpContext& dc) const;
 #endif
 
-protected:  // ¿Ø¼şÌõÇ¶Èë³ÉÔ±
+protected:  // æ§ä»¶æ¡åµŒå…¥æˆå‘˜
 	CStatusBar  m_wndStatusBar;
 	CToolBar    m_wndToolBar;
 
-// Éú³ÉµÄÏûÏ¢Ó³Éäº¯Êı
+// ç”Ÿæˆçš„æ¶ˆæ¯æ˜ å°„å‡½æ•°
 protected:
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	DECLARE_MESSAGE_MAP()
